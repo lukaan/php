42f3ecef41e8ffc33ab56f87794a802e0388b856
Luca Kang <kangkr01@gmail.com>
2021-02-15 20:41:45 -0300

    aula 02 php com git

---
+++ aula02.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+Comentários + de 1 linha
+*/
+
+//Comentário de apenas 1 linha
+
+$nomeSobrenome = "Luca Kang";
+echo $nomeSobrenome."<br/>";
+
+//Tipos de dados
+/*
+Inteiro
+Ponto Flutuante = Double, Float
+String, Variável
+Array 
+Objeto
+Booleanos = Verdadeiro ou Falso
+*/
+
+$idade = 19;
+$pi = 3.14;
+$nomeSobrenome = "Luca Kang";
+$acesso = true;
+
+echo $idade."<br/>";
+echo $pi."<br/>"; 
+echo $nomeSobrenome."<br/>";
+echo $acesso."<br/>";
+
+
+
+?>
