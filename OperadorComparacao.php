dbbe01fd803693feeddb1c764d5bc0d26fd41443
Luca Kang <kangkr01@gmail.com>
2021-02-18 22:46:13 -0300

    aula 03 operadores php

---
+++ OperadorComparacao.php
@@ -0,0 +1,14 @@
+<?php 
+
+$nota = 2;
+$notaTrabalho = 8;
+
+
+$resultado = $nota !== $notaTrabalho;
+
+$xpto = boolval($resultado) ? 'true' : 'false';
+
+
+echo "Resultado = ".$xpto;
+
+?>
---
+++ operadores.php
@@ -0,0 +1,14 @@
+<?php
+
+//Operadores Aritméticos
+$numero = 8;
+$numero1 = 10;
+
+$resultado = $numero % $numero1;
+
+//Texto concatenado com uma variável, o (.) é a concatenação.
+echo "Resultado = ".$resultado;
+
+
+
+?>
